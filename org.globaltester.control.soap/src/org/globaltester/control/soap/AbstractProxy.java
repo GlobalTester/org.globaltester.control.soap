c9be15494c20bb2dddd0569da1464755ffc9a8bc
Alexander May <alexander.may@hjp-consulting.com>
2016-06-07 15:03:56 +0200

    refactor c.h.g* to o.g.*

---
+++ org.globaltester.control.soap/src/org/globaltester/control/soap/AbstractProxy.java
@@ -0,0 +1,21 @@
+package org.globaltester.control.soap;
+
+/**
+ * This is the root of all SOAP proxy implementations. The type parameter T
+ * should be the object providing the actual functionality.
+ * 
+ * @author mboonk
+ *
+ * @param <T>
+ */
+public abstract class AbstractProxy<T> {
+	protected T handler;
+
+	public AbstractProxy(T handler) {
+		this.handler = handler;
+	}
+
+	public T getHandler() {
+		return handler;
+	}
+}
---
+++ org.globaltester.control.soap/src/org/globaltester/control/soap/SimulatorControlSoapProxy.java
@@ -0,0 +1,59 @@
+package org.globaltester.control.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+import com.hjp.simulator.SimulatorControl;
+
+/**
+ * SOAP proxy for {@link SimulatorControl} objects.
+ * @author mboonk
+ *
+ */
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class SimulatorControlSoapProxy extends AbstractProxy<SimulatorControl> implements SimulatorControl {
+	
+	public SimulatorControlSoapProxy(SimulatorControl handler) {
+		super(handler);
+	}
+	
+	@Override
+	@WebMethod
+	public boolean startSimulator() {
+		return handler.startSimulator();
+	}
+
+	@Override
+	@WebMethod
+	public boolean stopSimulator() {
+		return handler.stopSimulator();
+	}
+
+	@Override
+	@WebMethod
+	public boolean loadConfiguration(String configurationIdentifier) {
+		return handler.loadConfiguration(configurationIdentifier);
+	}
+
+	@Override
+	@WebMethod
+	public boolean updateState(String updateType, String[] updateParameters) {
+		return handler.updateState(updateType, updateParameters);
+	}
+
+	@Override
+	@WebMethod
+	public String[] getState(String stateType, String[] stateParameters) {
+		return handler.getState(stateType, stateParameters);
+	}
+
+	@Override
+	@WebMethod
+	public String getError() {
+		return handler.getError();
+	}
+
+}
